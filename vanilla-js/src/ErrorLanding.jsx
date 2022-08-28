e7c0d6481f5e7f9d5bcdca365eb954cb651418a8
IGardocki <ian.d.gardocki@gmail.com>
2022-08-28 17:34:02 -0500

    added comments to start to implement unit converter later

--- vanilla-js/src/ErrorLanding.jsx
+++ vanilla-js/src/ErrorLanding.jsx
@@ -6,6 +6,19 @@ export const ErrorLanding = () => {
             <h1>404 Error: The Meal You Are Looking For Was Not Found!</h1>
             <p>Have you considered giving our meal of the day a try?</p>
             <RandomMeal />
+            <div>
+
+{/* FROM IAN: TRYING TO IMPLEMENT A SIMPLE UNIT CONVERTER. THIS IS DEFINITELY A STRETCH GOAL */}
+
+            {/* https://www.theunitconverter.com/unit-conversion-widget.html#:~:text=Entry%20the%20required%20value%2C%20it%20will%20get%20the,into%20the%20desired%20position%20in%20your%20websites%20code. */}
+
+            {/* <!-- Unit Conversion Script - TheUnitConverter.com /--> */}
+            
+            {/* <div style="width:238px;margin:0px auto;"><div style="width:99.5%;border:1px solid #2D6AB4;border-top:none;border-bottom:none;text-align:center; height:35px;font-size:16px;padding:5px 0px 0px 0px;border-top-right-radius:5px; border-top-left-radius:5px;background-color:#2D6AB4; font-weight:bold;"><a href="https://www.theunitconverter.com/volume-conversion/" style="text-decoration:none;color:#FFFFFF;" rel="nofollow">Conversion Widget</a></div><script type="text/javascript" src="https://ww.theunitconverter.com/converter.php?l=en&c=0&a=FFFFFF&b=2D6AB4&s=volume"></script></div> */}
+            
+            {/* <!-- End of Unit Conversion Script --> */}
+
+            </div>
         </>
     )
 }
