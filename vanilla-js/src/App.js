d7123a0936524e986ca5883dcc14f07739551bc2
IGardocki <ian.d.gardocki@gmail.com>
2022-08-29 22:07:26 -0500

    small updates to submenu

--- vanilla-js/src/App.js
+++ vanilla-js/src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, useContext } from 'react'
 import { BrowserRouter as Router, Route, Routes, Link, } from 'react-router-dom'
-import styled from 'styled-components'
+
 import { MealContext } from './MealContext';
 import { Header } from './Header'
 import { Homepage } from './Homepage'
